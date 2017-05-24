ee4097b3b0d75e349592d89811398f2f58f46ff9
Ron <ron.eyal.eng@gmail.com>
2017-05-24 15:12:01 +0300

    add gradient thresholds

--- camera_calib.py
+++ camera_calib.py
@@ -55,7 +55,41 @@ class camera_calib():
             plt.show()
 
 
+    # Define a function that takes an image, number of x and y points,
+    # camera matrix and distortion coefficients
+    def corners_unwarp(img, nx, ny, mtx, dist):
+        # Use the OpenCV undistort() function to remove distortion
+        undist = cv2.undistort(img, mtx, dist, None, mtx)
+        # Convert undistorted image to grayscale
+        gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+        # Search for corners in the grayscaled image
+        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
 
+        if ret == True:
+            # If we found corners, draw them! (just for fun)
+            cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
+            # Choose offset from image corners to plot detected corners
+            # This should be chosen to present the result at the proper aspect ratio
+            # My choice of 100 pixels is not exact, but close enough for our purpose here
+            offset = 100  # offset for dst points
+            # Grab the image shape
+            img_size = (gray.shape[1], gray.shape[0])
+
+            # For source points I'm grabbing the outer four detected corners
+            src = np.float32([corners[0], corners[nx - 1], corners[-1], corners[-nx]])
+            # For destination points, I'm arbitrarily choosing some points to be
+            # a nice fit for displaying our warped result
+            # again, not exact, but close enough for our purposes
+            dst = np.float32([[offset, offset], [img_size[0] - offset, offset],
+                              [img_size[0] - offset, img_size[1] - offset],
+                              [offset, img_size[1] - offset]])
+            # Given src and dst points, calculate the perspective transform matrix
+            M = cv2.getPerspectiveTransform(src, dst)
+            # Warp the image using OpenCV warpPerspective()
+            warped = cv2.warpPerspective(undist, M, img_size)
+
+        # Return the resulting image and matrix
+        return warped, M
 
 
 if __name__ == '__main__':
@@ -74,52 +108,3 @@ if __name__ == '__main__':
     img = cv2.imread('camera_cal/calibration2.jpg')
     camera_calib.undistort(img, plot=True)
 
-    '''
-
-    #object points for a single image
-    objp = np.zeros((nx * ny, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
-
-    #object points for all images
-    object_points = []
-    #image points for all images
-    img_points = [] #np.array(dtype=np.float32)
-
-    for fname in images:
-
-        img = cv2.imread(fname)
-        #plt.imshow(img)
-        print(img.shape)
-
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-        if ret == True:
-            object_points.append(objp)
-            img_points.append(corners)
-            #print (object_points)
-
-
-    img = cv2.imread('camera_cal/calibration2.jpg')
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_size = (img.shape[1], img.shape[0])
-
-    object_points = np.array(object_points, dtype=np.float32)
-    print(object_points.shape)
-    #img_points = np.array(img_points, dtype=np.float32)
-    retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(object_points, img_points, img.shape[0:2], None, None)
-    undist = cv2.undistort(img, cameraMatrix, distCoeffs, None, cameraMatrix)
-
-
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    f.tight_layout()
-    ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(undist)
-    ax2.set_title('Undistorted Image', fontsize=50)
-    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
-    plt.show()
-'''
---
+++ gradient_thresholds.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def apply_threshold(img, thresh=(0,255)):
+    masked = np.zeros_like(img)
+    masked[(img >= thresh[0]) & (img <= thresh[1])] = 1
+    return masked
+
+def apply_sobel(gray, orient='x', sobel_kernel=3):
+    if orient == 'x':
+        grad = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    else:
+        grad = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    return grad
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Calculate directional gradient
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    grad = apply_sobel(gray, orient, sobel_kernel)
+    #plt.imshow(grad)
+    grad_binary = apply_threshold(grad, thresh=thresh)
+    return grad_binary
+
+def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    grad_x = apply_sobel(gray, 'x', sobel_kernel)
+    grad_y = apply_sobel(gray, 'y', sobel_kernel)
+    magnitude = np.sqrt(grad_x**2 + grad_y**2)
+    # Apply threshold
+    mag_binary = apply_threshold(magnitude, mag_thresh)
+    return mag_binary
+
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    grad_x = apply_sobel(gray, 'x', sobel_kernel)
+    grad_y = apply_sobel(gray, 'y', sobel_kernel)
+    angles = np.arctan2(grad_y, grad_x)
+    # Apply threshold
+    dir_binary = apply_threshold(angles, thresh)
+    return dir_binary
+
+def hls_threshold(image, channel='S', thresh=(0,255)):
+    #Change color map
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    if channel == 'H':
+        channel_idx = 0
+    elif channel == 'L':
+        channel_idx = 1
+    else:
+        channel_idx = 2
+    single_channel = hls[:,:,channel_idx]
+    binary = np.zeros_like(single_channel)
+    binary[(single_channel > thresh[0]) & (single_channel <= thresh[1])] = 1
+    return binary
+
+if __name__ == '__main__':
+
+    # Choose a Sobel kernel size
+    ksize = 5 # Choose a larger odd number to smooth gradient measurements
+
+    image = mpimg.imread('test_images/test2.jpg')
+    # Apply each of the thresholding functions
+    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(200, 3000))
+    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(100, 3000))
+    #grady = np.zeros_like(grady)
+    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(300, 1000))
+    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(np.pi * 0.15, np.pi * 0.30))
+    dir_binary[dir_threshold(image, sobel_kernel=ksize, thresh=(np.pi * 0.65, np.pi * 0.85)) == 1] = 1
+    dir_binary[dir_threshold(image, sobel_kernel=ksize, thresh=(-np.pi * 0.85, -np.pi * 0.65)) == 1] = 1
+    dir_binary[dir_threshold(image, sobel_kernel=ksize, thresh=(-np.pi * 0.30, -np.pi * 0.15)) == 1] = 1
+
+    s_binary = hls_threshold(image, 'S', thresh=(90, 255))
+
+    combined = np.zeros_like(dir_binary)
+    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (s_binary==1)] = 1
+
+    combined_grad = np.zeros_like(dir_binary)
+    combined_grad[((gradx == 1) & (grady == 1))] = 1
+
+    combined_mag = np.zeros_like(dir_binary)
+    combined_mag[((mag_binary == 1) & (dir_binary == 1))] = 1
+
+    f, subplots = plt.subplots(3, 3, figsize=(24, 9))
+    f.tight_layout()
+    subplots[0,0].imshow(image)
+    subplots[0,0].set_title('Original Image', fontsize=10)
+    subplots[1,0].imshow(combined, cmap='gray')
+    subplots[1,0].set_title('All Combined Thresholds', fontsize=10)
+
+    subplots[2,0].imshow(s_binary, cmap='gray')
+    subplots[2,0].set_title('Threshold S channel', fontsize=10)
+    subplots[0,2].imshow(gradx, cmap='gray')
+    subplots[0,2].set_title('Thresholded X Gradient', fontsize=10)
+    subplots[1,2].imshow(grady, cmap='gray')
+    subplots[1,2].set_title('Thresholded Y Gradient', fontsize=10)
+    subplots[2,2].imshow(combined_grad, cmap='gray')
+    subplots[2,2].set_title('Combined X and Y Gradient', fontsize=10)
+
+    subplots[0,1].imshow(mag_binary, cmap='gray')
+    subplots[0,1].set_title('Thresholded Gradient Magnitude', fontsize=10)
+    subplots[1,1].imshow(dir_binary, cmap='gray')
+    subplots[1,1].set_title('Thresholded Gradient Direction', fontsize=10)
+    subplots[2,1].imshow(combined_mag, cmap='gray')
+    subplots[2,1].set_title('Combined Magnitude and Angle', fontsize=10)
+
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    plt.show()
